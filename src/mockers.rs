8554fb6ebab674d6f304dacbc8c4064d9524a315
inxaneninja <inxaneninja@gmail.com>
2021-05-11 16:26:40 +0200

    1. Added ClosureMocker 2. Added more documentation

--- src/mockers.rs
+++ src/mockers.rs
@@ -6,16 +6,17 @@ pub struct AlternatingMocker {
 }
 
 impl AlternatingMocker {
+    /// Creates a new AlternatingMocker. This by default starts with a lowercase letter, if you wanna control that, use `AlternatingMocker::new_from_bool()`.
     pub fn new() -> Self {
         Self {
             current: false
         }
     }
-}
-
-impl Default for AlternatingMocker {
-    fn default() -> Self {
-        Self::new()
+    /// Creates a new AlternatingMocker, and allows you to control the case of the starting letter with the parameter.
+    pub fn new_from_bool(starting_case: bool) -> Self {
+        Self {
+            current: starting_case
+        }
     }
 }
 
@@ -30,14 +31,9 @@ impl Mocker for AlternatingMocker {
 pub struct RandomMocker;
 
 impl RandomMocker {
+    /// Creates a new RandomMocker.
     pub fn new() -> Self {
         Self {}
-    }
-}
-
-impl Default for RandomMocker {
-    fn default() -> Self {
-        Self::new()
     }
 }
 
@@ -47,3 +43,24 @@ impl Mocker for RandomMocker {
     }
 }
 
+/// A built-in Mocker, which takes in a closure that returns a boolean. Useful if you don't feel like implementing the Mocker trait yourself.
+pub struct ClosureMocker<T: Fn() -> bool> {
+    closure: T
+}
+
+impl<T: Fn() -> bool> ClosureMocker<T> {
+    /// Creates a new ClosureMocker.
+    /// The parameter must be a closure which returns a `bool`.
+    /// The closure will be executed to determine whether the letter should be uppercase or not.
+    pub fn new(closure: T) -> Self {
+        Self {
+            closure: closure
+        }
+    }
+}
+
+impl<T: Fn() -> bool> Mocker for ClosureMocker<T> {
+    fn uppercase(&mut self) -> bool {
+        (self.closure)()
+    }
+}
